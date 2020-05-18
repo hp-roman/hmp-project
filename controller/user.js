3641dd25397ae02cdc4b8b3214703fa39bfe99b3
= <=>
2020-05-18 16:37:09 +0700

    Update user forgot password

--- controller/user.js
+++ controller/user.js
@@ -160,7 +160,9 @@ exports.forgetPassword = async (req, res, next) => {
     if (!email.match(/[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim)) {
       return res.json({ success: false, message: "Email không đúng!!!" });
     }
+    console.log(process.env.EMAIL, process.env.PASSWORD);
     const transporter = nodemailer.createTransport({
+      pool: true,
       host: "smtp.gmail.com",
       port: 465,
       secure: true,
