111f808e038845b92d028f3faf1f91c3180bef14
= <=>
2020-05-18 10:42:29 +0700

    Update function forget password

--- controller/user.js
+++ controller/user.js
@@ -160,12 +160,15 @@ exports.forgetPassword = async (req, res, next) => {
       return res.json({ success: false, message: "Email không đúng!!!" });
     }
     const transporter = nodemailer.createTransport({
-      service: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      service: "gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.user,
         pass: process.env.pass,
+      },
+      tls: {
+        rejectUnauthorized: false,
       },
     });
     const mailOptions = {
