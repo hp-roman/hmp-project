75b4536cf2b2c0c129603e38136041378627700c
Hoang Phi <34290027+brucelee7@users.noreply.github.com>
2020-05-24 00:17:29 +0700

    Add feedback

--- router/user.js
+++ router/user.js
@@ -7,5 +7,6 @@ router.post('/login', controller.login);
 router.post('/change', controller.changePassword);
 router.post('/forget', controller.forgetPassword);
 router.get('/reset', controller.resetPassword);
+router.get('/feedback', controller.feedback);
 
 module.exports = router;
